df37bcdbefd5f750a9f2bef3f05311b1d6f61ee4
David Gomez <contact@davidgomez.dev>
2024-01-04 14:43:20 -0500

    Enable Vercel’s speed insights
    https://vercel.com/docs/speed-insights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from 'next'
+import { SpeedInsights } from "@vercel/speed-insights/next"
 import { GeistSans } from "geist/font/sans";
 import { Italiana } from 'next/font/google'
 import './globals.css'
@@ -21,7 +22,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${GeistSans.variable} ${italiana.variable}`}>{children}</body>
+      <body className={`${GeistSans.variable} ${italiana.variable}`}>
+        {children}
+        <SpeedInsights />
+      </body>
     </html>
   )
 }
