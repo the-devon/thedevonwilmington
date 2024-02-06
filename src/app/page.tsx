14e97294d215b74195ad3ed693d27fe5e00240db
David Gomez <code@davidgomez.dev>
2024-02-06 17:28:33 -0500

    Use new components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,17 @@
-import Image from 'next/image';
-import { Navigation } from '@/components/navigation';
+import { Hero, Navigation, Neighborhood, Overview } from '@/components';
 
 export default function Home() {
   return (
-    <>
+    <div className="grid grid-cols-[1fr_min(75ch,_calc(100%_-_2rem))_1fr] gap-8 text-lg font-light leading-6 tracking-wide">
       <Navigation />
-      <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
-    </>
+
+      <Hero />
+
+      <main className="col-span-1 col-start-2">
+        <Overview />
+
+        <Neighborhood />
+      </main>
+    </div>
   );
 }
