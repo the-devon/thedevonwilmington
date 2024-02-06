120465b908fd61ba15dbb919912b21f708ad272f
David Gomez <code@davidgomez.dev>
2024-02-06 17:27:47 -0500

    Create Overview section

---
+++ src/components/overview.tsx
@@ -0,0 +1,46 @@
+export function Overview() {
+  return (
+    <section className="my-20">
+      <header className="mb-10 mt-20">
+        <h1 className="my-4 text-center font-display text-5xl md:text-7xl">
+          The Devon Condominium
+        </h1>
+        <h3 className="my-4 text-center text-2xl font-thin italic md:text-3xl">
+          Wilmington, DE
+        </h3>
+      </header>
+      <p className="my-4">
+        Welcome to The Devon, an esteemed mid-century modern condominium
+        community gracing Pennsylvania Avenue in the heart of Wilmington, DE.
+        Nestled between the enchanting neighborhoods of Wawaset Park and The
+        Highlands, our distinguished 15-story building seamlessly merges the
+        vibrancy of city living with the serenity of tranquil surroundings.
+      </p>
+      <p className="my-4">
+        As a resident of The Devon, you will uncover a trove of amenities
+        enhancing your daily life. A doorman awaits to extend a warm welcome,
+        setting the tone for the exceptional experience that awaits you. During
+        the summer, luxuriate in our freshly refreshed outdoor pool or treat
+        yourself to cultural delights at nearby gems like Rockford Park,
+        Greenville, or Brandywine Park—all effortlessly within reach.
+      </p>
+      <p className="my-4">
+        Indulge in the convenience of our amenities complemented by an attentive
+        on-site management team. Monthly condominium fees cover a spectrum of
+        services, from all essential utilities to Xfinity cable service,
+        ensuring your worry-free living experience. With The Devon as your home,
+        every detail is meticulously crafted to elevate your living experience.
+      </p>
+      <p className="my-4">
+        Immerse yourself in a seamless blend of urban sophistication and
+        suburban tranquility, all within easy reach of local attractions such as
+        Rockford Park, Delaware Art Museum, Trolley Square, and the Academy of
+        Life-Long Learning. Your extraordinary city living journey unfolds
+        effortlessly at The Devon, where architecturally renowned Trolley Square
+        and nature-adorned Brandywine Park beckon you to explore. Welcome to a
+        residence where luxury, comfort, and cultural richness converge, with
+        The Devon as your haven in the heart of Wilmington.
+      </p>
+    </section>
+  );
+}
